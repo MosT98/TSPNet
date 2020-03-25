28111fe05994ea1c315ff770f1ace17357e8d371
Andrei Halauca <ahalauca@gmail.com>
2020-03-25 18:44:11 +0200

    added lab6

---
+++ LabPostSiComment/ClientFORMS/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ClientFORMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgp = new System.Windows.Forms.DataGridView();
+            this.testPostCommentDataSet = new ClientFORMS.TestPostCommentDataSet();
+            this.postsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.postsTableAdapter = new ClientFORMS.TestPostCommentDataSetTableAdapters.PostsTableAdapter();
+            this.postIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.domainDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgc = new System.Windows.Forms.DataGridView();
+            this.postsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.testPostCommentDataSet1 = new ClientFORMS.TestPostCommentDataSet1();
+            this.commentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.commentsTableAdapter = new ClientFORMS.TestPostCommentDataSet1TableAdapters.CommentsTableAdapter();
+            this.commentIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.postPostIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testPostCommentDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.postsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.postsBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testPostCommentDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.commentsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgp
+            // 
+            this.dgp.AutoGenerateColumns = false;
+            this.dgp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgp.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.postIdDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.domainDataGridViewTextBoxColumn,
+            this.dateDataGridViewTextBoxColumn});
+            this.dgp.DataSource = this.postsBindingSource;
+            this.dgp.Location = new System.Drawing.Point(12, 35);
+            this.dgp.Name = "dgp";
+            this.dgp.Size = new System.Drawing.Size(445, 150);
+            this.dgp.TabIndex = 0;
+            // 
+            // testPostCommentDataSet
+            // 
+            this.testPostCommentDataSet.DataSetName = "TestPostCommentDataSet";
+            this.testPostCommentDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // postsBindingSource
+            // 
+            this.postsBindingSource.DataMember = "Posts";
+            this.postsBindingSource.DataSource = this.testPostCommentDataSet;
+            // 
+            // postsTableAdapter
+            // 
+            this.postsTableAdapter.ClearBeforeFill = true;
+            // 
+            // postIdDataGridViewTextBoxColumn
+            // 
+            this.postIdDataGridViewTextBoxColumn.DataPropertyName = "PostId";
+            this.postIdDataGridViewTextBoxColumn.HeaderText = "PostId";
+            this.postIdDataGridViewTextBoxColumn.Name = "postIdDataGridViewTextBoxColumn";
+            this.postIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            // 
+            // domainDataGridViewTextBoxColumn
+            // 
+            this.domainDataGridViewTextBoxColumn.DataPropertyName = "Domain";
+            this.domainDataGridViewTextBoxColumn.HeaderText = "Domain";
+            this.domainDataGridViewTextBoxColumn.Name = "domainDataGridViewTextBoxColumn";
+            // 
+            // dateDataGridViewTextBoxColumn
+            // 
+            this.dateDataGridViewTextBoxColumn.DataPropertyName = "Date";
+            this.dateDataGridViewTextBoxColumn.HeaderText = "Date";
+            this.dateDataGridViewTextBoxColumn.Name = "dateDataGridViewTextBoxColumn";
+            // 
+            // dgc
+            // 
+            this.dgc.AutoGenerateColumns = false;
+            this.dgc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgc.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.commentIdDataGridViewTextBoxColumn,
+            this.postPostIdDataGridViewTextBoxColumn,
+            this.textDataGridViewTextBoxColumn});
+            this.dgc.DataSource = this.commentsBindingSource;
+            this.dgc.Location = new System.Drawing.Point(12, 221);
+            this.dgc.Name = "dgc";
+            this.dgc.Size = new System.Drawing.Size(445, 150);
+            this.dgc.TabIndex = 1;
+            // 
+            // postsBindingSource1
+            // 
+            this.postsBindingSource1.DataMember = "Posts";
+            this.postsBindingSource1.DataSource = this.testPostCommentDataSet;
+            // 
+            // testPostCommentDataSet1
+            // 
+            this.testPostCommentDataSet1.DataSetName = "TestPostCommentDataSet1";
+            this.testPostCommentDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // commentsBindingSource
+            // 
+            this.commentsBindingSource.DataMember = "Comments";
+            this.commentsBindingSource.DataSource = this.testPostCommentDataSet1;
+            // 
+            // commentsTableAdapter
+            // 
+            this.commentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // commentIdDataGridViewTextBoxColumn
+            // 
+            this.commentIdDataGridViewTextBoxColumn.DataPropertyName = "CommentId";
+            this.commentIdDataGridViewTextBoxColumn.HeaderText = "CommentId";
+            this.commentIdDataGridViewTextBoxColumn.Name = "commentIdDataGridViewTextBoxColumn";
+            this.commentIdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // postPostIdDataGridViewTextBoxColumn
+            // 
+            this.postPostIdDataGridViewTextBoxColumn.DataPropertyName = "PostPostId";
+            this.postPostIdDataGridViewTextBoxColumn.HeaderText = "PostPostId";
+            this.postPostIdDataGridViewTextBoxColumn.Name = "postPostIdDataGridViewTextBoxColumn";
+            // 
+            // textDataGridViewTextBoxColumn
+            // 
+            this.textDataGridViewTextBoxColumn.DataPropertyName = "Text";
+            this.textDataGridViewTextBoxColumn.HeaderText = "Text";
+            this.textDataGridViewTextBoxColumn.Name = "textDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgc);
+            this.Controls.Add(this.dgp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testPostCommentDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.postsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.postsBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testPostCommentDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.commentsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgp;
+        private TestPostCommentDataSet testPostCommentDataSet;
+        private System.Windows.Forms.BindingSource postsBindingSource;
+        private TestPostCommentDataSetTableAdapters.PostsTableAdapter postsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn postIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn domainDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dgc;
+        private System.Windows.Forms.BindingSource postsBindingSource1;
+        private TestPostCommentDataSet1 testPostCommentDataSet1;
+        private System.Windows.Forms.BindingSource commentsBindingSource;
+        private TestPostCommentDataSet1TableAdapters.CommentsTableAdapter commentsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn commentIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn postPostIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn textDataGridViewTextBoxColumn;
+    }
+}
+
