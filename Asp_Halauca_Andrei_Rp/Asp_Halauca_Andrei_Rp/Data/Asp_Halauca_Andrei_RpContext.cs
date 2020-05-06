c88ba62a139319e675e3f795c04b57a9eb8250d5
Andrei Halauca <ahalauca@gmail.com>
2020-05-06 13:10:08 +0300

    Added razor lab

---
+++ Asp_Halauca_Andrei_Rp/Asp_Halauca_Andrei_Rp/Data/Asp_Halauca_Andrei_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Halauca_Andrei_Rp.Model;
+
+namespace Asp_Halauca_Andrei_Rp.Data
+{
+    public class Asp_Halauca_Andrei_RpContext : DbContext
+    {
+        public Asp_Halauca_Andrei_RpContext (DbContextOptions<Asp_Halauca_Andrei_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Halauca_Andrei_Rp.Model.Movie> Movie { get; set; }
+    }
+}
---
+++ Asp_Halauca_Andrei_Rp/Asp_Halauca_Andrei_Rp/Migrations/20200505142539_AddedRating.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Asp_Halauca_Andrei_Rp.Migrations
+{
+    public partial class AddedRating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+        }
+    }
+}
