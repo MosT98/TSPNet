2eda0e1bc9ea7f09e104c93b149a4ff665312706
Andrei Halauca <ahalauca@gmail.com>
2020-05-13 16:01:14 +0300

    Lab modified with razor pages

---
+++ LabPostSiComment/RazorPagesMovie/Models/CommentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class CommentDTO
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public int PostPostId { get; set; }
+        public string Text { get; set; }
+        public virtual PostDTO Post { get; set; }
+    }
+}
---
+++ LabPostSiComment/RazorPagesMovie/Models/PostDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class PostDTO
+    {
+        public PostDTO()
+        {
+            this.Comments = new HashSet<CommentDTO>();
+        }
+
+        [Key]
+        public int PostId { get; set; }
+
+        public string Description { get; set; }
+        public string Domain { get; set; }
+        public System.DateTime Date { get; set; }
+
+        public virtual ICollection<CommentDTO> Comments { get; set; }
+    }
+}
